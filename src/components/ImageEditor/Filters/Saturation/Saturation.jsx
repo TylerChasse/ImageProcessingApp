348053a68545d3a699f6c52b46fa592a3753f5c8
Ryan Jackson <ryan.jackson@mymail.champlain.edu>
2025-11-11 16:15:49 -0500

    updated saturation before and after images in editor

--- src/components/ImageEditor/Filters/Saturation/Saturation.jsx
+++ src/components/ImageEditor/Filters/Saturation/Saturation.jsx
@@ -1,13 +1,14 @@
 import FilterTemplate from '../FilterTemplate';
-
+import saturationBefore from '../../../../assets/saturationBefore.jpg'
+import saturationAfter from '../../../../assets/saturationBefore.jpg'
 const Saturation = () => {
   return (
     <FilterTemplate
       filterId="saturation"
       filterName="Saturation"
       description="Adjusts the intensity of colors in the image. Lower values create a more muted, grayscale look, while higher values make colors more vibrant."
-      beforeImage={null}  // TODO: Add actual before/after example images
-      afterImage={null}
+      beforeImage={saturationBefore}
+      afterImage={saturationAfter}
     />
   );
 };
