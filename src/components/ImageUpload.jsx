60ad6334ac4030437dc993d450cd2697a96467fd
Tyler Chasse <22chast@gmail.com>
2025-11-12 13:18:47 -0500

    document imageupload.jsx

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -1,3 +1,9 @@
+/**
+ * ImageUpload Component
+ * 
+ * Reusable button that lets users upload images.
+ */
+
 import { useRef } from 'react';
 import { useImage } from '../context/ImageContext';
 
@@ -5,12 +11,15 @@ const ImageUpload = ({ buttonText = 'Upload a Photo', buttonClassName = '' }) =>
   const { handleImageUpload } = useImage();
   const fileInputRef = useRef(null);
 
+  // Open file picker when button is clicked
   const handleButtonClick = () => {
     fileInputRef.current?.click();
   };
 
+  // Handle file selection
   const handleFileChange = (e) => {
     const file = e.target.files?.[0];
+    // Only accept image files
     if (file && file.type.startsWith('image/')) {
       handleImageUpload(file);
     }
@@ -21,6 +30,7 @@ const ImageUpload = ({ buttonText = 'Upload a Photo', buttonClassName = '' }) =>
       <button className={buttonClassName} onClick={handleButtonClick}>
         {buttonText}
       </button>
+      {/* Hidden file input */}
       <input
         ref={fileInputRef}
         type="file"
