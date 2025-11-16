7c0963247ebe115eeb6c6ae98a965d42403d4999
Tyler Chasse <22chast@gmail.com>
2025-11-16 17:31:06 -0500

    added emboss example

--- src/components/ImageEditor/Filters/Emboss/Emboss.jsx
+++ src/components/ImageEditor/Filters/Emboss/Emboss.jsx
@@ -1,4 +1,6 @@
 import FilterTemplate from '../FilterTemplate';
+import embossBefore from '../../../../assets/emboss/download (1).jpg';
+import embossAfter from '../../../../assets/emboss/download (1)-edited.png';
 
 const Emboss = () => {
   return (
@@ -6,8 +8,8 @@ const Emboss = () => {
       filterId="emboss"
       filterName="Emboss"
       description="Replace each pixel with a highlight or shadow based on the light/dark areas. Used to create a 3D effect or to enhance text."
-      beforeImage={null}  // TODO: Add actual before/after example images
-      afterImage={null}
+      beforeImage={embossBefore}  // TODO: Add actual before/after example images
+      afterImage={embossAfter}
     />
   );
 };
